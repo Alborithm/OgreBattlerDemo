f07c6846c9226df650c56d8985689f8d0d95e4a8
Alborithm <47321228+Alborithm@users.noreply.github.com>
2020-03-10 13:26:43 +0100

    Add WPF and Database SQL
    Add WPF Application for Desktop UI
    Add Database SQL project To store OB Data

---
+++ OgreBattlerDemo/WPFOBD/Bootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Caliburn.Micro;
+using WPFOBD.ViewModels;
+
+namespace WPFOBD
+{
+    // Video Example of implementation https://www.youtube.com/watch?v=p-fmOJMLl0A&list=PLLWMQd6PeGY0bEMxObA6dtYXuJOGfxSPx&index=6
+
+    class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+    }
+}
